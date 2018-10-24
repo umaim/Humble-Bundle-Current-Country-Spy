fc7581c5a3603bffa76cc4c388d6a27ab71113eb
Cloud <cloudswift@qq.com>
2018-10-24 10:03:13 +0800

    Fix require dom-ready

--- HBCCS.user.js
+++ HBCCS.user.js
@@ -4,40 +4,38 @@
 // @author       Cloud
 // @namespace    https://github.com/PixvIO/Humble-Bundle-Current-Country-Spy
 // @supportURL   https://github.com/PixvIO/Humble-Bundle-Current-Country-Spy/issues
-// @version      1.1.3
+// @version      1.1.4
 // @downloadURL  https://raw.githubusercontent.com/PixvIO/Humble-Bundle-Current-Country-Spy/master/HBCCS.user.js
 // @icon         https://humblebundle-a.akamaihd.net/static/hashed/46cf2ed85a0641bfdc052121786440c70da77d75.png
 // @include      https://www.humblebundle.com/*
+// @grant        unsafeWindow
 // @run-at       document-end
 // ==/UserScript==
 
 (function () {
     'use strict';
-    require(['lib/require-domReady'], function (domReady) {
-        domReady(function () {
-            const showCountry = () => {
-                try {
-                    //const country = window.models.request.country_code;
-                    const country = document.head.innerText.match(/country_code: "\w{2}"/g).toString().slice(15, 17) || '';
 
-                    let node = document.getElementsByClassName('tabs tabs-navbar-item js-tabs-navbar-item js-maintain-scrollbar-on-dropdown');
+    const showCountry = () => {
+        try {
+            const country = unsafeWindow.models.request.country_code;
+            //const country = document.head.innerText.match(/country_code: "\w{2}"/g).toString().slice(15, 17) || '';
 
-                    if (node.item(0)) {
-                        let elem = document.createElement('span');
-                        elem.setAttribute('class', 'navbar-item navbar-item js-hide-dropdown not-dropdown button-title');
-                        elem.innerText = `${country}`;
-                        node.item(0).append(elem);
-                    }
-                } catch (e) {
-                    setTimeout(() => {
-                        showCountry();
-                    }, 500);
-                }
-            };
+            let node = document.getElementsByClassName('tabs tabs-navbar-item js-tabs-navbar-item js-maintain-scrollbar-on-dropdown');
 
+            if (node.item(0)) {
+                let elem = document.createElement('span');
+                elem.setAttribute('class', 'navbar-item navbar-item js-hide-dropdown not-dropdown button-title');
+                elem.innerText = `${country}`;
+                node.item(0).append(elem);
+            }
+        } catch (e) {
             setTimeout(() => {
                 showCountry();
-            }, 1);
-        });
-    });
+            }, 500);
+        }
+    };
+
+    setTimeout(() => {
+        showCountry();
+    }, 100);
 })();
