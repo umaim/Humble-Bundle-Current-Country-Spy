cf5cd206928657717c55acd10ba7d2ee3f6bbb67
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2019-10-06 13:59:35 +0800

    fix display on phone.

--- HBCCS.user.js
+++ HBCCS.user.js
@@ -4,7 +4,7 @@
 // @author       Cloud
 // @namespace    https://github.com/xPixv/Humble-Bundle-Current-Country-Spy
 // @supportURL   https://github.com/xPixv/Humble-Bundle-Current-Country-Spy/issues
-// @version      1.1.5
+// @version      1.1.6
 // @downloadURL  https://github.com/xPixv/Humble-Bundle-Current-Country-Spy/raw/master/HBCCS.user.js
 // @icon         https://humblebundle-a.akamaihd.net/static/hashed/46cf2ed85a0641bfdc052121786440c70da77d75.png
 // @include      https://www.humblebundle.com/*
@@ -18,14 +18,14 @@
     const showCountry = () => {
         try {
             const country = unsafeWindow.models.request.country_code;
-            //const country = document.head.innerText.match(/country_code: "\w{2}"/g).toString().slice(15, 17) || '';
 
             let node = document.getElementsByClassName('tabs tabs-navbar-item js-tabs-navbar-item js-maintain-scrollbar-on-dropdown');
 
             if (node.item(0)) {
-                let elem = document.createElement('span');
-                elem.setAttribute('class', 'navbar-item navbar-item js-hide-dropdown not-dropdown button-title');
-                elem.innerText = `${country}`;
+                let elem = document.createElement('a');
+                elem.setAttribute('class', 'navbar-item not-dropdown button-title');
+                elem.href = "#";
+                elem.innerHTML = '<span class="navbar-icon-text-wrapper"><i class="navbar-item-icon hb hb-star-o mobile"></i><span class="navbar-item-text">'+country+'</span></span>';
                 node.item(0).append(elem);
             }
         } catch (e) {
